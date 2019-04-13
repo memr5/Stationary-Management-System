7e7220523556fdcfde6b946290ed13952d580286
Mr5 <meetranoliya094@gmail.com>
2019-04-13 20:37:49 +0530

    Removed Menu method

--- src/StationaryPackage/Pages.java
+++ src/StationaryPackage/Pages.java
@@ -4,7 +4,6 @@ import java.sql.SQLException;
 
 public interface Pages {
     void Banner();
-    void Menu();
     void WelcomePage();
     boolean Login() throws SQLException;
     boolean Sign_up() throws SQLException;
