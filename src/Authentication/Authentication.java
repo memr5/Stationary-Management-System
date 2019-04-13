a8991ad364ec59c1a971e458c27afdebbb0b90a1
Vatsal <vatsal.pict17@sot.pdpu.ac.in>
2019-04-14 00:07:23 +0530

    completed : 1)Add code to detect if user exist or not 2)Add code to new user record

--- src/Authentication/Authentication.java
+++ src/Authentication/Authentication.java
@@ -5,7 +5,7 @@ import java.sql.*;
 public class Authentication {
     public static Connection connect() throws SQLException{
         Connection conn = DriverManager.getConnection(
-                "jdbc:mysql://localhost:3306/Stationary-DB","root","memr5");
+                "jdbc:mysql://localhost:3306/Stationary-DB","root","Vatsal@1999");
 
         return conn;
     }
