a6a44928d01a1f8883e023d78da7244679a73490
Mr5 <meetranoliya094@gmail.com>
2019-04-15 02:17:32 +0530

    Authentication class fully implemented

--- src/Authentication/Authentication.java
+++ src/Authentication/Authentication.java
@@ -10,16 +10,11 @@ public class Authentication {
     }
 
     public static boolean isAdmin(String user_name) throws Exception{
-        Class.forName("com.mysql.cj.jdbc.Driver");
+
         Connection connection = (new Authentication()).connect();
         Statement statement = connection.createStatement();
         ResultSet rst = statement.executeQuery("SELECT type from user where user_name = '" + user_name + "'");
-        //connection.close();
-        if(rst.next() && rst.getInt(1) == 0){
-            //connection.close();
-            return false;
-        }
-        //connection.close();
-        return true;
+
+        return !(rst.next() && rst.getInt(1) == 0);
     }
 }
