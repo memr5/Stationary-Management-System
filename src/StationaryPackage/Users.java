55c12837eddc23a842760ac5a42c80b42758e48f
Vatsal Parsaniya <33985480+vatsalparsaniya@users.noreply.github.com>
2019-04-14 15:18:15 +0530

    Search Method implemented

--- src/StationaryPackage/Users.java
+++ src/StationaryPackage/Users.java
@@ -32,15 +32,6 @@ class Users {
                 "\t\tEnter your choice : ");
     }
 
-    void Search(){
-        Scanner in = new Scanner(System.in);
-        System.out.println("\t\tEnter product type : ");
-        String type = in.nextLine();
-        //Search product of this type | Display Results
-        // with index numbers | (-1) to get Back to Main menu
-
-    }
-
     void orders()throws Exception{
         int user_id = (new StationaryClass()).getUser_id();
         ResultSet rst = statement.executeQuery("SELECT time_stamp,product_id,quantity from history where user_id = " +
@@ -142,6 +133,121 @@ class Users {
                 break;
             }
         }while(true);
+    }
+
+    void show_product_type(){
+
+        try {
+            Connection con = Authentication.connect();
+            ResultSet rst;
+            Statement st = con.createStatement();
+            rst = st.executeQuery("SELECT DISTINCT type_of_product FROM product ");
+            while (rst.next()) {
+                System.out.println(rst.getString(1));
+            }
+            con.close();
+        }catch (Exception ex){
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    void  purchase_item() {
+        float After_Discount_Price;
+        int buy_quantity;
+        int break_loop1 = 0;
+        int break_loop2 = 0;
+        int break_loop3 = 0;
+        int break_loop4 = 0;
+        int item_Number = 0;
+        do {
+
+            this.show_product_type();
+            System.out.print("Which type of product you want : ");
+            Scanner sc = new Scanner(System.in);
+            String product_type = sc.nextLine();
+            String upper_product_type = product_type.toUpperCase();
+            try {
+                Connection con = Authentication.connect();
+                Statement st = con.createStatement();
+                int index = 1;
+                do {
+                    ResultSet rst = st.executeQuery("SELECT * FROM product WHERE type_of_product = '" + upper_product_type + "'");
+                    while (rst.next()) {
+                        System.out.println(index++ + ")    " + rst.getString(3) + "[" + rst.getInt(8) + "]");
+                        System.out.println("        Selling Price : " + rst.getInt(5));
+                        System.out.println("        Discount : " + rst.getInt(6));
+                        After_Discount_Price = (float) rst.getInt(5) - (float) (rst.getInt(5) * rst.getInt(6)) / 100;
+                        System.out.println("        After Discount Price : " + After_Discount_Price + "\n\n");
+                    }
+                    rst.beforeFirst();
+                    System.out.println("Select your choice : \n      1) Select item\n      2) Back");
+                    int choice_1 = sc.nextInt();
+                    do {
+                        switch (choice_1) {
+                            case 1:
+                                System.out.println("Select item : ");
+                                item_Number = sc.nextInt();
+                                break_loop4 = 1;
+                                break;
+                            case 2:
+                                break_loop3 = 1;
+                                break;
+                            default:
+                                System.out.println("please enter valid number");
+                        }
+                    } while (break_loop3 != 1 || break_loop4 != 1);
+
+
+                    if (break_loop3 == 1) {
+                        break;
+                    }
+
+                    for (int i = 0; i < item_Number; i++) {
+                        rst.next();
+                    }
+
+                    After_Discount_Price = (float) rst.getInt(5) - (float) (rst.getInt(5) * rst.getInt(6)) / 100;
+                    System.out.println("You Select : \n");
+                    System.out.println(rst.getString(3) + "[" + rst.getInt(8) + "]");
+                    System.out.println("        Selling Price : " + rst.getInt(5));
+                    System.out.println("        Discount : " + rst.getInt(6));
+                    System.out.println("        After Discount Price : " + After_Discount_Price + "\n\n");
+                    System.out.println("        Specification : " + rst.getString(7));
+                    System.out.println("\n      1) Add TO Cart \n       2) back\n\n      Enter Your answer :");
+                    int selection = sc.nextInt();
+
+                    do {
+                        switch (selection) {
+                            case 1:
+                                //customer buy this item
+                                do {
+                                    System.out.print("Add Quantity : ");
+                                    buy_quantity = sc.nextInt();
+                                    if (buy_quantity > rst.getInt(8)) {
+                                        System.out.println("Sorry We have only " + rst.getInt(8) + " Quantity");
+                                    }
+                                } while (buy_quantity > rst.getInt(8));
+
+                                addToCart(rst.getInt(1), buy_quantity);
+                                System.out.println("Added To Cart SuccessFully......\n");
+                                break_loop2 = 1;
+                                break;
+                            case 2:
+                                //customer want to go Back
+                                break_loop1 = 1;
+                                break;
+                            default:
+                                System.out.println("Please Select appropriate Option");
+                        }
+                    } while (break_loop1 != 1 || break_loop2 != 1);
+
+                } while (break_loop2 != 1);
+
+            } catch (Exception ex) {
+                System.out.println(ex);
+            }
+        } while (break_loop2 != 1);
+
     }
 
     Connection getConnection(){
