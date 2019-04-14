abdff2e216523be49e498b9fdd78ebb91056c201
Mr5 <meetranoliya094@gmail.com>
2019-04-15 02:21:19 +0530

    User class fully implemented

--- src/StationaryPackage/Users.java
+++ src/StationaryPackage/Users.java
@@ -23,7 +23,7 @@ class Users {
     }
 
     void Menu(){
-        System.out.print("---------------------------------MENU--------------------------------\n\n" +
+        System.out.print("\n---------------------------------MENU--------------------------------\n" +
                 "\t\t1. Search\n" +
                 "\t\t2. Your Orders\n" +
                 "\t\t3. Cart\n" +
@@ -33,49 +33,53 @@ class Users {
     }
 
     void orders()throws Exception{
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        int user_id = (new StationaryClass()).getUser_id();
-        ResultSet rst = statement.executeQuery("SELECT time_stamp,product_id,quantity from history where user_id = " +
-                "\"" + user_id + "\"");
+
+        int user_id = StationaryClass.getUser_id();
+        System.out.println("\nPurchase History : ");
+        ResultSet rst = statement.executeQuery("SELECT time_stamp,product_id,quantity from history where user_id = " + user_id);
         while (rst.next()){
-            ResultSet pname = statement.executeQuery("SELECT product_name from product where product_id = " +
-                    "\"" + rst.getInt(2) + "\"");
+            Statement s = connection.createStatement();
+            ResultSet pname = s.executeQuery("SELECT product_name from product where product_id = " + rst.getInt(2));
             pname.next();
-            System.out.println("Time : " + rst.getTimestamp(1) + "\nProduct Name : " + pname.getString(1) +
-                    "\nQuantity = " + rst.getInt(3));
+            System.out.println("\nTime : " + rst.getTimestamp(1) + "\nProduct Name : " + pname.getString(1) +
+                    "\nQuantity : " + rst.getInt(3));
         }
 
     }
 
     private void addToCart(Integer product_id, Integer quantity) throws Exception{
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        ResultSet rst = statement.executeQuery("SELECT selling_price,discount from product where product_id = " + "\"" +
-                product_id + "\"");
+
+        ResultSet rst = statement.executeQuery("SELECT selling_price,discount from product where product_id = " +
+                product_id );
         rst.next();
-        cartValue +=(rst.getInt(1)-rst.getInt(2))*quantity;
+        cartValue +=(rst.getInt(1)-(rst.getInt(1)*rst.getInt(2)/100))*quantity;
         cart.add(product_id);
         this.quantity.add(quantity);
     }
 
     private void removeFromCart(int product_index)throws Exception{
 
-        ResultSet rst = statement.executeQuery("SELECT selling_price,discount from product where product_id = " + "\"" +
-                cart.get(product_index) + "\"");
+        ResultSet rst = statement.executeQuery("SELECT selling_price,discount from product where product_id = " +
+                cart.get(product_index));
         rst.next();
-        cartValue -= (rst.getInt(1)-rst.getInt(2))*quantity.get(product_index);
+        cartValue -= (rst.getInt(1)-(rst.getInt(1)*rst.getInt(2)/100))*quantity.get(product_index);
         cart.remove(product_index);
         quantity.remove(product_index);
     }
 
-    void checkout() throws Exception{
-        Class.forName("com.mysql.cj.jdbc.Driver");
+    private void checkout() throws Exception{
+
+        if(cart.size()==0){
+            System.out.println("\nEmpty Cart!\n");
+            return;
+        }
         for (int i=0;i<cart.size();i++){
-            ResultSet rst = statement.executeQuery("SELECT quantity from product where product_id = " + "\"" +
-                    cart.get(i) + "\"");
+            ResultSet rst = statement.executeQuery("SELECT quantity from product where product_id = " +
+                    cart.get(i) );
             rst.next();
             int new_quantity = rst.getInt(1) - quantity.get(i);
-            statement.executeUpdate("update product set quantity =" + new_quantity + "where product_id = " + "\"" +
-                    cart.get(i) + "\"");
+            statement.executeUpdate("update product set quantity = " + new_quantity + " where product_id = " +
+                    cart.get(i) );
         }
         updateSellingHistory();
         cart.clear();
@@ -85,16 +89,16 @@ class Users {
     }
 
     private void updateSellingHistory() throws Exception{
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        int user_id = (new StationaryClass()).getUser_id();
+
+        int user_id = StationaryClass.getUser_id();
+
         for(int i=0;i<cart.size();i++){
-            ResultSet rst = statement.executeQuery("select actual_price,selling_price,discount from product where product_id = " +
-                    "\"" + cart.get(i) + "\"");
+            ResultSet rst = statement.executeQuery("select actual_price,selling_price,discount from product where product_id = " + cart.get(i));
             rst.next();
             Timestamp tmp = new Timestamp(System.currentTimeMillis());
-            int profit = (rst.getInt(2)-rst.getInt(3)-rst.getInt(1))*quantity.get(i);
-            statement.executeUpdate("insert into history(time_stamp,user_id,product_id,quantity,profit) values("
-            + tmp +"," +user_id + "," + cart.get(i) + "," + quantity.get(i) + "," + profit + ")");
+            int profit = (rst.getInt(2) - (rst.getInt(2)*rst.getInt(3)/100) - rst.getInt(1))*quantity.get(i);
+            statement.executeUpdate("insert into history(time_stamp,user_id,product_id,quantity,profit) values" + "(\""
+            + tmp + "\"," + user_id + "," + cart.get(i) + "," + quantity.get(i) + "," + profit + ")");
         }
     }
 
@@ -102,19 +106,24 @@ class Users {
         int choice;
         Scanner in = new Scanner(System.in);
         do {
-            System.out.println("CART : ");
-            for (int i = 0; i < cart.size(); i++) {
-                System.out.println(i + 1 + " " + cart.get(i));
+            System.out.println("\nYour CART : ");
+            if(cart.size()==0){
+                System.out.println("(Cart is Empty)");
             }
-            System.out.print(cart.size() + 1 + " " + "Checkout\n" +
-                    cart.size() + 2 + " " + "Back to menu\n" +
+            for (int i = 0; i < cart.size(); i++) {
+                ResultSet rst = statement.executeQuery("select product_name from product where product_id = " + cart.get(i));
+                rst.next();
+                System.out.println(i+1 + ") " + rst.getString(1) + " | Quantity : " + quantity.get(i));
+            }
+            System.out.print("---------------------------------------------------------------------\n" + (cart.size()+1) + ") " + "Checkout\n" +
+                    (cart.size() + 2) + ") " + "Back to menu\n" +
                     "Choice : ");
 
             choice = in.nextInt();
             if (choice > 0 && choice <= cart.size()) {
-                System.out.print(  "1 " + "Remove item\n" +
-                                   "2 " + "Change Quantity\n" +
-                                   "3 " + "Back\n" +
+                System.out.print(  "1) " + "Remove item\n" +
+                                   "2) " + "Change Quantity\n" +
+                                   "3) " + "Back\n" +
                                    "Choice : ");
                 int c = in.nextInt();
                 switch (c){
@@ -125,15 +134,26 @@ class Users {
                     case 2:
                         System.out.print("Enter new quantity : ");
                         int q = in.nextInt();
-                        quantity.set(choice-1, q);
-                        System.out.println("\nChanged!\n");
+                        ResultSet rst = statement.executeQuery("select quantity from product where product_id = "
+                        + cart.get(choice-1));
+                        rst.next();
+                        if(rst.getInt(1) >= q){
+                            quantity.set(choice-1, q);
+                            System.out.println("\nChanged!\n");
+                        }
+                        else {
+                            System.out.println("Only " + rst.getInt(1) + " Available");
+                        }
                         break;
                     case 3:
                         break;
+                    default:
+                        System.out.println("\nEnter valid choice!\n");
                 }
             }
-            else if(choice == cart.size() + 1){
+            else if(choice == cart.size()+1){
                 checkout();
+                break;
             }
             else{
                 break;
@@ -141,8 +161,8 @@ class Users {
         }while(true);
     }
 
-    private void show_product_type() throws Exception{
-        Class.forName("com.mysql.cj.jdbc.Driver");
+    private void show_product_type(){
+        System.out.println("\nTypes of products available : \n");
         try {
             ResultSet rst = statement.executeQuery("SELECT DISTINCT type_of_product FROM product ");
             while (rst.next()) {
@@ -153,40 +173,49 @@ class Users {
         }
     }
 
-    void  purchase_item() throws Exception{
-        Class.forName("com.mysql.cj.jdbc.Driver");
+    void  purchase_item(){
+
         float After_Discount_Price;
         int buy_quantity;
+
         int break_loop1 = 0;
         int break_loop2 = 0;
         int break_loop3 = 0;
         int break_loop4 = 0;
         int item_Number = 0;
-        do {
 
+        do {
             this.show_product_type();
-            System.out.print("Which type of product you want : ");
+            System.out.println("\n-1) Back to Menu");
+            System.out.print("\nWhich type of product you want : ");
+
             Scanner sc = new Scanner(System.in);
             String product_type = sc.nextLine();
+
+            if(product_type.equals("-1")){
+                break;
+            }
+
             String upper_product_type = product_type.toUpperCase();
+
             try {
-                int index = 1;
                 do {
+                    int index = 1;
                     ResultSet rst = statement.executeQuery("SELECT * FROM product WHERE type_of_product = '" + upper_product_type + "'");
                     while (rst.next()) {
-                        System.out.println(index++ + ")    " + rst.getString(3) + "[" + rst.getInt(8) + "]");
-                        System.out.println("        Selling Price : " + rst.getInt(5));
-                        System.out.println("        Discount : " + rst.getInt(6));
+                        System.out.println("\n" + index++ + ")    " + rst.getString(3) + "[" + rst.getInt(8) + "]");
+                        System.out.println("\t\tSelling Price : " + rst.getInt(5));
+                        System.out.println("\t\tDiscount : " + rst.getInt(6));
                         After_Discount_Price = (float) rst.getInt(5) - (float) (rst.getInt(5) * rst.getInt(6)) / 100;
-                        System.out.println("        After Discount Price : " + After_Discount_Price + "\n\n");
+                        System.out.println("\t\tAfter Discount Price : " + After_Discount_Price + "\n\n");
                     }
                     rst.beforeFirst();
-                    System.out.println("Select your choice : \n      1) Select item\n      2) Back");
+                    System.out.print("Select your choice :\n\t\t1) Select item\n\t\t2) Back\n\t\tEnter your choice : ");
                     int choice_1 = sc.nextInt();
                     do {
-                        switch (choice_1) {
+                        switch(choice_1){
                             case 1:
-                                System.out.println("Select item : ");
+                                System.out.print("Select item : ");
                                 item_Number = sc.nextInt();
                                 break_loop4 = 1;
                                 break;
@@ -196,10 +225,10 @@ class Users {
                             default:
                                 System.out.println("please enter valid number");
                         }
-                    } while (break_loop3 != 1 || break_loop4 != 1);
+                    }while(break_loop3!=1 && break_loop4!=1);
 
 
-                    if (break_loop3 == 1) {
+                    if (break_loop3 == 1 || item_Number>=index) {
                         break;
                     }
 
@@ -208,19 +237,19 @@ class Users {
                     }
 
                     After_Discount_Price = (float) rst.getInt(5) - (float) (rst.getInt(5) * rst.getInt(6)) / 100;
-                    System.out.println("You Select : \n");
-                    System.out.println(rst.getString(3) + "[" + rst.getInt(8) + "]");
-                    System.out.println("        Selling Price : " + rst.getInt(5));
-                    System.out.println("        Discount : " + rst.getInt(6));
-                    System.out.println("        After Discount Price : " + After_Discount_Price + "\n\n");
-                    System.out.println("        Specification : " + rst.getString(7));
-                    System.out.println("\n      1) Add TO Cart \n       2) back\n\n      Enter Your answer :");
+                    System.out.println("\n\nYou Selected : \n");
+                    System.out.println("\t\t" + rst.getString(3) + " [" + rst.getInt(8) + "]");
+                    System.out.println("\t\tSelling Price : " + rst.getInt(5));
+                    System.out.println("\t\tDiscount : " + rst.getInt(6));
+                    System.out.println("\t\tAfter Discount Price : " + After_Discount_Price + "\n\n");
+                    System.out.println("\t\tSpecification : " + rst.getString(7));
+                    System.out.print("\n\t\t1) Add TO Cart\n\t\t2) back\n\n\t\tEnter Your answer : ");
+
                     int selection = sc.nextInt();
 
                     do {
                         switch (selection) {
                             case 1:
-                                //customer buy this item
                                 do {
                                     System.out.print("Add Quantity : ");
                                     buy_quantity = sc.nextInt();
@@ -234,18 +263,17 @@ class Users {
                                 break_loop2 = 1;
                                 break;
                             case 2:
-                                //customer want to go Back
                                 break_loop1 = 1;
                                 break;
                             default:
                                 System.out.println("Please Select appropriate Option");
                         }
-                    } while (break_loop1 != 1 || break_loop2 != 1);
+                    } while (break_loop1 != 1 && break_loop2 != 1);
 
                 } while (break_loop2 != 1);
 
             } catch (Exception ex) {
-                System.out.println(ex.getMessage());
+                System.out.println(ex.toString());
             }
         } while (break_loop2 != 1);
 
