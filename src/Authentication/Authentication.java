68dd9dbcbbb386e937f123d80802e095d3f61a6b
Mr5 <meetranoliya094@gmail.com>
2019-04-14 15:57:20 +0530

    Added isAdmin in Authentication

--- src/Authentication/Authentication.java
+++ src/Authentication/Authentication.java
@@ -3,13 +3,20 @@ package Authentication;
 import java.sql.*;
 
 public class Authentication {
+
     public static Connection connect() throws SQLException{
         return DriverManager.getConnection(
-                "jdbc:mysql://localhost:3306/Stationary-DB","root","memr5");
+                "jdbc:mysql://127.0.0.1:3306/stationary-db","root","memr5");
     }
 
-    public static boolean isAdmin(String user_name){
-        //Add code to check if user_name is of Admin or not
+    public static boolean isAdmin(String user_name) throws Exception{
+        Connection connection = Authentication.connect();
+        Statement statement = connection.createStatement();
+        ResultSet rst = statement.executeQuery("SELECT type from user where user_name = '" + user_name + "'");
+        rst.next();
+        if(rst.getInt(1) == 0){
+            return false;
+        }
         return true;
     }
 }
